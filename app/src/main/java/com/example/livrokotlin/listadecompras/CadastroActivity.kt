80364c1dd47b6ff3b470b836f2b64a02bb3f09e3
Your Name <you@example.com>
2020-06-07 17:18:55 -0300

    biblioteca Anko

--- app/src/main/java/com/example/livrokotlin/listadecompras/CadastroActivity.kt
+++ app/src/main/java/com/example/livrokotlin/listadecompras/CadastroActivity.kt
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_cadastro.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.toast
 
 class CadastroActivity : AppCompatActivity() {
 
@@ -30,16 +32,33 @@ class CadastroActivity : AppCompatActivity() {
             //verificando se o usuário digitou algum valor
 
             if (produto.isNotEmpty() && qtd.isNotEmpty() && valor.isNotEmpty()) {
-                //enviando item para lista - val prod monta o objeto que está sendo salvo
-                val prod = Produto(produto, qtd.toInt(), valor.toDouble(), imageBitmap)
-                produtosGlobal.add(prod)
+                //inserindo o produto no banco de dados
 
-                txt_produto.text.clear()
-                txt_qtd.text.clear()
-                txt_valor.text.clear()
+                database.use {
+
+                    val idProduto = insert("Produtos",
+                        "nome" to produto,
+                        "quantidade" to qtd,
+                        "valor" to valor.toDouble(),
+                        "foto" to imageBitmap?.toByteArray()
+                    )
+
+                    if (idProduto != -1L) {
+                        toast("Item inserido com sucesso")
+
+                        txt_produto.text.clear()
+                        txt_qtd.text.clear()
+                        txt_valor.text.clear()
+
+                    } else {
+
+                        toast("Erro ao inserir no banco de dados")
+                    }
+                }
 
             } else {
-                txt_produto.error = if (txt_produto.text.isEmpty()) "Preencha o nome do produto" else null
+                txt_produto.error =
+                    if (txt_produto.text.isEmpty()) "Preencha o nome do produto" else null
                 txt_qtd.error = if (txt_qtd.text.isEmpty()) "Preencha a quantidade" else null
                 txt_valor.error = if (txt_valor.text.isEmpty()) "Preencha o valor" else null
             }
@@ -47,12 +66,12 @@ class CadastroActivity : AppCompatActivity() {
 
         //pegando as imagens escolhidas pelo usuário
 
-        img_foto_produto.setOnClickListener{
+        img_foto_produto.setOnClickListener {
             abrirGaleria()
         }
     }
 
-    fun abrirGaleria(){
+    fun abrirGaleria() {
         //definindo a ação de conteúdo
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         //definindo filtro para imagens
@@ -65,8 +84,8 @@ class CadastroActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == COD_IMAGE && resultCode == Activity.RESULT_OK){
-            if(data != null){
+        if (requestCode == COD_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
 
                 //lendoa a uri com a imagem
                 val inputStream = data.getData()?.let { contentResolver.openInputStream(it) }
@@ -78,7 +97,5 @@ class CadastroActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }
 

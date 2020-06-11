e59c79e09b097a82fe4ea9a770d831e1ac4fbf09
Your Name <you@example.com>
2020-06-11 12:02:18 -0300

    add e removendo registros

--- app/src/main/java/com/example/livrokotlin/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/livrokotlin/listadecompras/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.livrokotlin.listadecompras
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.NumberFormat
+import org.jetbrains.anko.db.parseList
+import org.jetbrains.anko.db.rowParser
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import java.text.NumberFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +25,19 @@ class MainActivity : AppCompatActivity() {
         //definindo o adapter na lista
         list_view_produtos.adapter = produtosAdapter
 
+        list_view_produtos.setOnItemLongClickListener{
+                adapterView: AdapterView<*>, view1: View, position: Int, id: Long ->
+            //buscando o item clicado
+            val item = produtosAdapter.getItem(position)
+            produtosAdapter.remove(item)
+            if (item != null) {
+                deletarProduto(item.id)
+            }
+            toast("item deletado com sucesso")
+
+            true
+        }
+
         btn_adicionar.setOnClickListener {
             startActivity<CadastroActivity>()
 //            val intent = Intent(this, CadastroActivity::class.java)
@@ -29,14 +48,40 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         val adapter = list_view_produtos.adapter as? ProdutoAdapter
-            adapter?.clear()
-            adapter?.addAll(produtosGlobal)
 
-        val soma = produtosGlobal.sumByDouble { it.valor * it.quantidade }
-        val f = NumberFormat.getCurrencyInstance(Locale("pt", "br"))
-        txt_total.text = "TOTAL: ${f.format(soma)}"
+        database.use {
+            //efetuando uma consulta no banco de dados
+            select("produtos").exec {
+                //criando o parser que montará o produto
+                val parser = rowParser { id: Int, nome: String,
+                                         quantidade: Int,
+                                         valor: Double,
+                                         foto: ByteArray? ->
+                    //coluna do bando de dados
 
+                    //montagem do objeto Produyto com as colunas do banco
+                    Produto(id, nome, quantidade, valor, foto?.toBitmap())
+                }
+                //criando a lista de produtos com dados do banco
+                var listaProdutos = parseList(parser)
+
+                adapter?.clear()
+                adapter?.addAll(listaProdutos)
+
+                val soma = listaProdutos.sumByDouble { it.valor * it.quantidade }
+                val f = NumberFormat.getCurrencyInstance(Locale("pt", "br"))
+                txt_total.text = "TOTAL: ${f.format(soma)}"
+
+            }
+        }
+    }
+
+    fun deletarProduto(idProduto:Int){
+            var deleteProduto = arrayOf(idProduto.toString())
+        database.use{
+            delete("produtos", "id", deleteProduto)
         }
 
 
+    }
 }
